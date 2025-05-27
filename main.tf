5611b6c9915ebae5003c0cb423ed6256ee7a14c6
jaibuss2018 <jaibuss2018@yahoo.com>
2025-05-27 11:39:51 -0400

    additional storage

--- main.tf
+++ main.tf
@@ -36,3 +36,11 @@ resource "azurerm_storage_account" "storage" {
   location = azurerm_resource_group.rg.location
   
 }
+resource "azurerm_storage_account" "storage" {
+  name = "jaiterrastorage2"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  
+}
